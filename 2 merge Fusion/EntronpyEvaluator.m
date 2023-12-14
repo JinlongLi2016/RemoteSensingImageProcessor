6c0feead04f7c6de1f5a4efec79617b21b3c5dde
JinlongLi2016 <longtolin@foxmail.com>
2023-12-14 23:21:18 +0800

    改正熵累计时候的-(应该为+号),以及添加考试用得相对配置代码matcher3.m

--- 2 merge Fusion/EntronpyEvaluator.m
+++ 2 merge Fusion/EntronpyEvaluator.m
@@ -2,18 +2,18 @@
 % for ch = 1:1:3
 %     disp( AssessArray(t(:, :, ch)) );
 % end
+% 
+% wei_img = FusionHandlerBy('weight');% jiaquan
+% for ch = 1:1:3
+%     disp( AssessArray(wei_img(:, :, ch)) );
+% end
+% 
+% t =FusionHandlerBy('multiplication'); %chengji
+% for ch = 1:1:3
+%     disp( AssessArray(t(:, :, ch)) );
+% end
 
-wei_img = imread('加权融合.jpg'); % jiaquan
-for ch = 1:1:3
-    disp( AssessArray(wei_img(:, :, ch)) );
-end
-
-t = imread('乘积变换.jpg'); %chengji
-for ch = 1:1:3
-    disp( AssessArray(t(:, :, ch)) );
-end
-
-t = imread('比值变换融合.jpg'); %bizhi
+t = FusionHandlerBy('ratio'); %bizhi
 for ch = 1:1:3
     disp( AssessArray(t(:, :, ch)) );
 end
